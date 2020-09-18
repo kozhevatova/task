fcda795015142ed23a808a62779e24b8f3495a53
Anna Kozhevatova <ad.kozhevatova@yandex.ru>
2020-09-18 15:01:34 +0300

    refactored

--- task.js
+++ task.js
@@ -1,23 +1,17 @@
 const input = document.querySelector('.tel-form__input');
 const relatedLink = document.getElementById(input.dataset.relatedLink);
+const matrix = '+7 (___) ___-__-__';
+const defaultValue = matrix.replace(/\D/g, '');
+input.value = matrix;
 
-const setCursorPosition = (pos, elem) => {
-  elem.focus();
-  if (elem.setSelectionRange) {
-    elem.setSelectionRange(pos, pos);
-  } else if (elem.createTextRange) {
-    const range = elem.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', pos);
-    range.moveStart('character', pos);
-    range.select();
-  }
-};
+function changeRelatedLink(value) {
+  relatedLink.innerText = `Позвонить на ${value}`;
+  relatedLink.href = `tel:${value}`;
+  relatedLink.style.color = 'rgb(119,221,119)';
+}
 
 function mask(event) {
-  const matrix = '+7 (___) ___-__-__';
   let i = 0;
-  const defaultValue = matrix.replace(/\D/g, '');
   let currentValue = this.value.replace(/\D/g, '');
 
   if (defaultValue.length >= currentValue.length) {
@@ -25,27 +19,20 @@ function mask(event) {
   }
 
   this.value = matrix.replace(/./g, (tel) => {
-    let result = '';
     if (/[_\d]/.test(tel) && i < currentValue.length) {
-      result = currentValue.charAt(i++);
-    } else if (i >= currentValue.length) {
-      result = '';
-    } else {
-      result = tel;
+      return currentValue.charAt(i++);
+    } if (i >= currentValue.length) {
+      return '';
     }
-    return result;
+    return tel;
   });
 
   if (event.type === 'blur' && this.value.length === 2) {
     this.value = '';
-  } else {
-    setCursorPosition(this.value.length, this);
   }
 
   if (this.value.length === 18) {
-    relatedLink.innerText = `Позвонить на ${this.value}`;
-    relatedLink.href = `tel:${this.value}`;
-    relatedLink.style.color = 'rgb(119,221,119)';
+    changeRelatedLink(this.value);
   }
 }
 
